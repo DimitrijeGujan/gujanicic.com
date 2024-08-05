1c836d0d2c984928f7d061165d8d7c86bdc5c23b
Dimitrije <dgujanicic@college.harvard.edu>
2024-08-05 19:52:47 -0400

    Aleksa partial

---
+++ aleksa/script.js
@@ -0,0 +1,19 @@
+document.querySelector('#scroll-arrow').addEventListener('click', () => {
+    window.scrollTo(0, window.innerHeight);
+});
+
+// Animation for boxes
+window.addEventListener('scroll', () => {
+    const boxes = document.querySelectorAll('.box');
+    const triggerPoint = window.innerHeight / 1.3;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerPoint) {
+            box.classList.add('visible');
+        } else {
+            box.classList.remove('visible');
+        }
+    });
+});
